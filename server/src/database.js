8d927768e099c4e0b71a26ca263c8321ae91f0c1
Labo <labo@ds.juguel.com>
2021-11-10 15:46:37 -0600

    Se quitó la carpeta de server

--- server/src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/inmobiliaria_DB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log("Base de datos conectada!"))
-    .catch(err => console.log(err));
